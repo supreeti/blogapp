aa85d402d963571a9d762621248b801ed5077520
Supreeti Kushwaha <sksupreetk11@gmail.com>
2024-01-27 21:04:36 +0530

    add likes to post for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,28 @@ class PostsController < ApplicationController
 
   def show
     @users = User.find(params[:user_id])
-    @post = @users.posts.find(params[:id])
+    @posts = @users.posts.find(params[:id])
+  end
+
+  def new
+    @users = User.find(params[:user_id])
+    @posts = Post.new
+  end
+
+  def create
+    @posts = Post.new(posts_params)
+    @posts.author = current_user
+
+    if @posts.save
+      redirect_to user_post_path(user_id: current_user.id, id: @posts.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :text)
   end
 end
