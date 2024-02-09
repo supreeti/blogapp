6e7b0229350c7a9b73bb2ef41374ba2dfdae555e
Md.  Porag Sarkar <porag.m06@gmail.com>
2024-02-08 23:22:25 -0800

    initial spec added for defining user

--- spec/integration/users_show_spec.rb
+++ spec/integration/users_show_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+RSpec.describe 'Users#shows', type: :integration do
+  before(:all) do
+    Comment.delete_all
+    Post.delete_all
+    User.delete_all
+    @tom = User.create(name: 'Tom', photo: 'https://placehold.co/200x133', bio: 'Teacher from Mexico.',
+                       post_counter: 0)
+    Post.create(author: @tom, title: 'Hello', text: 'This is my first post', comment_counter: 0, like_counter: 0)
+    @lilly = User.create(name: 'Lilly', photo: 'https://placehold.co/200x133', bio: 'Teacher from Poland.',
+                         post_counter: 0)
+    @users = User.all
+  end
+end
