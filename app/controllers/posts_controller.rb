8367e4ab7672420b9e8e254da7016c7ce11f9977
Supreeti Kushwaha <sksupreetk11@gmail.com>
2024-02-13 20:31:12 +0530

    fix n+1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @post = @user.posts
+    @post = @user.posts.includes(:comments)
   end
 
   def show
